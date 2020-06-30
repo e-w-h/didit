e23cd51ba2529256362211f30d692a565bcb9429
Eric Hou <erichou24@gmail.com>
2020-06-30 03:20:34 -0400

    record date and time

--- main.go
+++ main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
+  "html/template"
   "net/http"
-  "os"
+  "time"
 )
 
+type Info struct {
+  Date string
+  Time string
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte("<h1>Hello World!</h1>"))
+  now := time.Now()
+  Clicked := Info {
+    Date: now.Format("01/02/2006"),
+    Time: now.Format("15:04:05"),
+  }
+  t, err := template.ParseFiles("index.html")
+  if err != nil {
+  }
+  err = t.Execute(w, Clicked)
+  if err != nil {
+  }
 }
 
 func main() {
-  port := os.Getenv("PORT")
-  if port == "" {
-    port = "3000"
-  }
-
-  mux := http.NewServeMux()
-
-  mux.HandleFunc("/", indexHandler)
-  http.ListenAndServe(":"+port, mux)
+  http.HandleFunc("/", indexHandler)
+  http.ListenAndServe(":3000", nil)
 }
