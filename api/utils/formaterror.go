80a6e1dcc1913d0712e710b9c7168cef3dedad46
Eric Hou <erichou24@gmail.com>
2020-07-06 04:29:11 -0400

    error handling

---
+++ api/utils/formaterror.go
@@ -0,0 +1,19 @@
+package formaterror
+
+import (
+  "errors"
+  "strings"
+)
+
+func FormatError(err string) error {
+  if strings.Contains(err, "username") {
+    return errors.New("Username Already Taken")
+  }
+  if strings.Contains(err, "email") {
+    return errors.New("Email Already Taken")
+  }
+  if strings.Contains(err, "hashedPassword") {
+    return errors.New("incorrect Password")
+  }
+  return errors.New("Incorrect Details")
+}
