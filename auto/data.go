b11a261185e79f6dd4d8c8c524d2ad6b8c9ec62b
Eric Hou <erichou24@gmail.com>
2020-07-08 23:46:24 -0400

    data

---
+++ auto/data.go
@@ -0,0 +1,7 @@
+package auto
+
+import "github.com/e-w-h/didit/api/models"
+
+var users = []models.User{
+  models.User{Username: "Dave Chappelle", Email: "davechappelle@gmail.com", Password: "password"}
+}
