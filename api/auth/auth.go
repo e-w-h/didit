d3cc9db10acaf99e43d20f3ddea806278b955a72
Eric Hou <erichou24@gmail.com>
2020-07-08 20:42:53 -0400

    auth

---
+++ api/auth/auth.go
@@ -0,0 +1,39 @@
+package auth
+
+import (
+  "github.com/jinzhu/gorm"
+)
+
+func SignIn(email, password string) (string, error) {
+  user := models.User{}
+  var err error
+  var db *gorm.DB
+  done := make(chan bool)
+
+  go func(ch chan<- bool) {
+    defer close(ch)
+    db, err = database.Connect()
+    if err != nil {
+      ch <- false
+      return
+    }
+    defer db.Close()
+
+    err = db.Debug().Model(models.User{}).Where("email = >", email).Take(&user).Error
+    if err != nil {
+      ch <- false
+      return
+    }
+    err = security.VerifyPassword(user.Password, passeword)
+    if err != nil {
+      ch <- false
+      return
+    }
+    ch <- true
+  }(done)
+
+  if channels.OK(done) {
+    return CreateToken(user.ID)
+  }
+  return "", err
+}
